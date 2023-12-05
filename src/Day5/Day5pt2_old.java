5a55025a60885e99cf55f64389137db2440d3f76
Rowena Sinclair <rowena@glassballoon.co.uk>
2023-12-05 22:18:39 +0000

    Day 5

--- src/Day5/Day5pt2_old.java
+++ src/Day5/Day5pt2_old.java
@@ -3,21 +3,37 @@ package Day5;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
+import java.util.*;
 
-public class Day5pt2 {
+public class Day5pt2_old {
 
     public static void main(String[] args) {
-
-        System.out.println(new Date());
 
 //        Path filePath = Paths.get("src/Input/Day4TestInput.txt");
         Path filePath = Paths.get("src/Input/Day4Input.txt");
         try {
             var lines = Files.readAllLines(filePath);
+
+            // first line will be seeds:
+            var seedsString = lines.get(0).substring(lines.get(0).indexOf(":")+2).split("\\ ");
+
+            var seedArray = new HashSet<Long>();
+
+            // ranges now of seeds
+            for (int i = 0; i < seedsString.length; i+=2){
+                System.out.println(i);
+                long start = Long.valueOf(seedsString[i]);
+                long range = Long.valueOf(seedsString[i+1]);
+                long end = start+range;
+                while (start <= end) {
+                    if (!seedArray.contains(start))
+                        seedArray.add(start);
+                    start++;
+                }
+            }
+
+
+            //var locationMaps = new ArrayList<LocationMapper>();
 
             int i = 3; //ignore seeds and blank line and header line
 
@@ -28,6 +44,7 @@ public class Day5pt2 {
                     i+=2;
                     break; // move to next section
                 }
+
                 seedToSoil.add(createLocationMapper(lines.get(i)));
                 i++;
             }
@@ -39,6 +56,7 @@ public class Day5pt2 {
                     i+=2;
                     break; // move to next section
                 }
+
                 soilToFertilizer.add(createLocationMapper(lines.get(i)));
                 i++;
             }
@@ -50,6 +68,7 @@ public class Day5pt2 {
                     i+=2;
                     break; // move to next section
                 }
+
                 fertilizerToWater.add(createLocationMapper(lines.get(i)));
                 i++;
             }
@@ -61,6 +80,7 @@ public class Day5pt2 {
                     i+=2;
                     break; // move to next section
                 }
+
                 waterToLight.add(createLocationMapper(lines.get(i)));
                 i++;
             }
@@ -72,6 +92,7 @@ public class Day5pt2 {
                     i+=2;
                     break; // move to next section
                 }
+
                 lightToTemp.add(createLocationMapper(lines.get(i)));
                 i++;
             }
@@ -83,6 +104,7 @@ public class Day5pt2 {
                     i+=2;
                     break; // move to next section
                 }
+
                 tempToHumidity.add(createLocationMapper(lines.get(i)));
                 i++;
             }
@@ -94,38 +116,22 @@ public class Day5pt2 {
                     i+=2;
                     break; // move to next section
                 }
+
                 humidityToLocation.add(createLocationMapper(lines.get(i)));
                 i++;
             }
 
-            // first line will be seeds:
-            var seedsString = lines.get(0).substring(lines.get(0).indexOf(":")+2).split("\\ ");
+            var seedList = seedArray.stream().toList();
 
-            long smallestNum = -1;
+            seedList = calcNextLocation(seedList, seedToSoil);
+            seedList = calcNextLocation(seedList, soilToFertilizer);
+            seedList = calcNextLocation(seedList, fertilizerToWater);
+            seedList = calcNextLocation(seedList, waterToLight);
+            seedList = calcNextLocation(seedList, lightToTemp);
+            seedList = calcNextLocation(seedList, tempToHumidity);
+            seedList = calcNextLocation(seedList, humidityToLocation);
 
-            // ranges now of seeds
-            for (int j = 0; j < seedsString.length; j+=2){
-                //System.out.println(j);
-                long start = Long.valueOf(seedsString[j]);
-                long range = Long.valueOf(seedsString[j+1]);
-                long end = start+range;
-                while (start <= end) {
-                    long currentLocation = start;
-                    currentLocation = calcNextLocation(currentLocation, seedToSoil);
-                    currentLocation = calcNextLocation(currentLocation, soilToFertilizer);
-                    currentLocation = calcNextLocation(currentLocation, fertilizerToWater);
-                    currentLocation = calcNextLocation(currentLocation, waterToLight);
-                    currentLocation = calcNextLocation(currentLocation, lightToTemp);
-                    currentLocation = calcNextLocation(currentLocation, tempToHumidity);
-                    currentLocation = calcNextLocation(currentLocation, humidityToLocation);
-
-                    if ((smallestNum == -1) || (currentLocation < smallestNum))
-                        smallestNum = currentLocation;
-                    start++;
-                }
-            }
-            System.out.println(new Date());
-            System.out.println(smallestNum);
+            System.out.println(seedList.stream().min(Long::compare).orElseThrow());
 
         } catch (Exception e){
             System.out.println(e);
@@ -139,16 +145,22 @@ public class Day5pt2 {
         return new LocationMapper(Long.parseLong(values[0]), Long.parseLong(values[1]), Long.parseLong(values[2]));
     }
 
-    static long calcNextLocation(long startingLocation, List<LocationMapper> locationMapper){
-        var mappedLocation = locationMapper.stream()
-                .filter(n->startingLocation >= n.getSourceStart()
-                        && startingLocation <= n.getSourceStart()+n.getRangeLength()).toList();
+    static List calcNextLocation(List<Long> seedArray, List<LocationMapper> locationMapper){
+        var lastestSeeds = new ArrayList<Long>();
+        for (int j = 0; j < seedArray.size(); j++){
+            long seedNum = seedArray.get(j);
+
+            var mappedLocation = locationMapper.stream()
+                    .filter(n->seedNum >= n.getSourceStart()
+                            && seedNum <= n.getSourceStart()+n.getRangeLength()).toList();
 
             // if nothing returned return itself
-        if (mappedLocation.size() != 0) {
-            var map = mappedLocation.get(0);
-            return map.getDestinationLocation(startingLocation);
-        } else return startingLocation;
+            if (mappedLocation.size() != 0) {
+                var map = mappedLocation.get(0);
+//                seedArray.set(j, map.getDestinationLocation(seedArray.get(j)));
+                lastestSeeds.add(map.getDestinationLocation(seedArray.get(j)));
+            } else lastestSeeds.add(seedNum);
+        }
+        return lastestSeeds.stream().distinct().toList();
     }
-
 }
